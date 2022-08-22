eaf15641f5d93c3c83dd1431da183a7e55bfea58
idlewith <newellzhou@163.com>
2022-08-23 01:04:02 +0800

    develop first step is done

---
+++ README.md
@@ -0,0 +1,67 @@
+
+# 安装Python
+
+下面是Python的直接下载地址，记得勾选加入环境变量，其他都是点击下一步
+
+https://repo.huaweicloud.com/python/3.9.7/python-3.9.7.exe
+
+
+# 配置镜像地址
+
+Windows路径为：C:\Users\<UserName>\pip\pip.ini
+
+```
+[global]
+index-url = https://repo.huaweicloud.com/repository/pypi/simple
+trusted-host = repo.huaweicloud.com
+timeout = 120
+```
+
+# 安装依赖
+
+```
+pip install -r requirements.txt
+```
+
+# 用法
+
+打开`template.xlsx`模板文件，在第一、二列输入事件的开始、结束时间即可
+
+运行命令
+
+```angular2html
+python calc_sla.py
+```
+ 
+结果会输出到`result.xlsx`文件
+
+# [可选] `config.json`配置文件
+
+可以修改午休、上下班时间和输入输出到excel文件名
+
+**午休时间**
+
+项|含义
+-|-
+launch_break_start|午休开始时间
+launch_break_end|午休结束时间
+launch_break_delta_hour|午休需要减掉到时间
+
+**上下班时间**
+
+项|含义
+-|-
+off_duty_first_start|下班开始时间
+off_duty_first_end|今天下班结束时间
+off_duty_second_start|第二天下班开始时间
+off_duty_second_end|第二天下班结束时间
+
+**输入输出到excel文件名**
+
+项|含义
+-|-
+input_excel_file_name|输入的excel文件名
+input_excel_sheet_name|输入的excel文件需要读取到Sheet名
+output_excel_file_name|输出的excel文件名
+
+
--- calc_sla.py
+++ calc_sla.py
@@ -13,6 +13,44 @@
 2022/5/3 09:30	2022/5/5 14:05	3155	52.6	4.1
 """
 import datetime
+from pprint import pprint
+import chinese_calendar as calendar
+import json
+import os
+from pprint import pprint
+from argparse import Namespace
+
+import openpyxl
+
+
+def load_break_time():
+    python_break_dict = {
+        "launch_break_start": "11:30:00",
+        "launch_break_end": "13:30:00",
+        "launch_break_delta_hour": 1.5,
+        "off_duty_first_start": "17:30:00",
+        "off_duty_first_end": "23:59:59",
+        "off_duty_second_start": "00:00:00",
+        "off_duty_second_end": "08:30:00",
+        "input_excel_file_name": "template.xlsx",
+        "input_excel_sheet_name": "Sheet1",
+        "output_excel_file_name": "result.xlsx"
+    }
+    json_file = 'config.json'
+    if not os.path.exists(json_file):
+        used_break_dict = python_break_dict
+    else:
+        json_break_dict = json.load(open(json_file))
+        same_dict = python_break_dict == json_break_dict
+        if same_dict:
+            used_break_dict = python_break_dict
+        else:
+            used_break_dict = json_break_dict
+    used_break_dict = Namespace(**used_break_dict)
+    return used_break_dict
+
+
+BREAK_DICT = load_break_time()
 
 
 class SlaModel:
@@ -34,8 +72,12 @@ class SlaModel:
 
 
 def str2datetime(date_str):
-    time_template = '%Y-%m-%d %H:%M:%S'
-    a = datetime.datetime.strptime(date_str + ':00', time_template)
+    if isinstance(date_str, datetime.datetime):
+        return date_str
+
+    time_template = '%Y/%m/%d %H:%M:%S'
+    # a = datetime.datetime.strptime(date_str + ':00', time_template)
+    a = datetime.datetime.strptime(date_str, time_template)
     return a
 
 
@@ -44,22 +86,268 @@ def calc_seconds(start_time, closed_time):
     return delta
 
 
-def calc_actual_time_delta(start_time, closed_time):
-    pass
+def get_hms(date):
+    h, m, s = date.hour, date.minute, date.second
+    return h, m, s
+
+
+def gen_datetime_from_break_hms(date_day, break_time_str):
+    y, m, d = get_ymd(date_day)
+
+    time_template = '%H:%M:%S'
+    break_start_hms = datetime.datetime.strptime(break_time_str, time_template)
+    h, mi, s = get_hms(break_start_hms)
+    return datetime.datetime(y, m, d, h, mi, s)
+
+
+def is_in_launch_break_time(start, close):
+    """
+    only focus this: close time contains launch break end
+    break time:     |----------------|
+    actual:     |-------------------------|
+    """
+
+    launch_break_start = gen_datetime_from_break_hms(start, BREAK_DICT.launch_break_start)
+    launch_break_end = gen_datetime_from_break_hms(start, BREAK_DICT.launch_break_end)
+    if start <= launch_break_start and close >= launch_break_end:
+        return True
+    else:
+        return False
+
+
+def is_in_off_duty_time(start, close):
+    """
+    在下班时间内解决了，就不能减去下班时间，
+    只有整个事件包含了整个下班时间
+        或者开始时间 > 下班时间并且结束时间 > 上班时间，才能减去下班时间
+
+    break time:     |-------||---------|
+    actual:     |-----------||------------|
+
+    break time: |---------||-------|
+    actual:         |-----||-------------------|
+    """
+    off_duty_first_start = gen_datetime_from_break_hms(start, BREAK_DICT.off_duty_first_start)
+    off_duty_first_end = gen_datetime_from_break_hms(start, BREAK_DICT.off_duty_first_end)
+    off_duty_second_start = gen_datetime_from_break_hms(close, BREAK_DICT.off_duty_second_start)
+    off_duty_second_end = gen_datetime_from_break_hms(close, BREAK_DICT.off_duty_second_end)
+
+    if start <= off_duty_first_start and off_duty_first_end == close:
+        return True, (off_duty_first_end - off_duty_first_start).total_seconds()
+
+    if start > off_duty_first_start and off_duty_first_end == close:
+        return True, (off_duty_first_end - start).total_seconds()
+
+    if start == off_duty_second_start and off_duty_second_end <= close:
+        return True, (off_duty_second_end - off_duty_second_start).total_seconds()
+
+    return False, 0
+
+    # if start <= off_duty_first_start and close <= off_duty_second_end:
+    #     return True, 15 * 60 * 60
+    # elif start >= off_duty_first_start and close >= off_duty_second_end:
+    #     return True, (off_duty_second_end - start).total_seconds()
+    # else:
+    #     return False, 0
+
+
+def calc_break_time(start, close):
+    need_minus_seconds_list = []
+
+    in_launch_break_time = is_in_launch_break_time(start, close)
+    if in_launch_break_time:
+        need_minus_seconds = BREAK_DICT.launch_break_delta_hour * 60 * 60
+        need_minus_seconds_list.append(need_minus_seconds)
+
+    in_off_duty_time, delta_minus_seconds_duty = is_in_off_duty_time(start, close)
+    if in_off_duty_time:
+        need_minus_seconds_list.append(delta_minus_seconds_duty)
+
+    return sum(need_minus_seconds_list)
+
+
+def calc_holiday_time(start, close):
+    return (close - start).total_seconds()
 
 
 def calc(start, close):
-    sla_model = SlaModel()
+    ys, ms, ds = get_ymd(start)
+    date_ymd = datetime.date(ys, ms, ds)
+    holiday = calendar.is_holiday(date_ymd)
+    workday = calendar.is_workday(date_ymd)
 
-    sla_model.start_time = str2datetime(start)
-    sla_model.closed_time = str2datetime(close)
-    sla_model.total_time_delta_min = int(calc_seconds(sla_model.start_time, sla_model.closed_time) / 60)
-    sla_model.total_time_delta_hour = int(sla_model.total_time_delta_min / 60)
-    sla_model.actual_time_delta_hour = calc_actual_time_delta(sla_model.start_time, sla_model.closed_time)
+    # 如果是节假日或者周末，累计的SLA时间为0
+    if holiday or not workday:
+        delta = calc_holiday_time(start, close)
+    else:
+        delta = calc_break_time(start, close)
+
+    return delta
+
+
+def is_same_day(start, close):
+    s = str2datetime(start)
+    c = str2datetime(close)
+    return get_ymd(s) == get_ymd(c)
+
+
+def get_ymd(date):
+    y, m, d = date.year, date.month, date.day
+    return y, m, d
+
+
+def concat_start_day_list(date_start):
+    y, m, d = get_ymd(date_start)
+    end_of_day = datetime.datetime(y, m, d, 23, 59, 59)
+    return [
+        date_start,
+        end_of_day
+    ]
+
+
+def concat_close_day_list(date_end):
+    y, m, d = get_ymd(date_end)
+    start_of_day = datetime.datetime(y, m, d, 0, 0, 0)
+    return [
+        start_of_day,
+        date_end
+    ]
+
+
+def concat_whole_day_list(date_middle):
+    y, m, d = get_ymd(date_middle)
+    start_of_day = datetime.datetime(y, m, d, 0, 0, 0)
+    end_of_day = datetime.datetime(y, m, d, 23, 59, 59)
+    return [
+        start_of_day,
+        end_of_day
+    ]
+
+
+def get_date_list_from_more_than_one_day(start, close):
+    date_list = []
+
+    s = str2datetime(start)
+    c = str2datetime(close)
+    days = (c - s).days
+
+    start_day_list = concat_start_day_list(s)
+    date_list.append(start_day_list)
+
+    for i in range(1, days):
+        day_middle = c - datetime.timedelta(days=i)
+        whole_day_list = concat_whole_day_list(day_middle)
+        date_list.append(whole_day_list)
+
+    close_day_list = concat_close_day_list(c)
+    date_list.append(close_day_list)
+
+    return date_list
+
+
+def calc_single_event(start, close):
+    sla_model = SlaModel()
+    sla_model.start_time = start
+    sla_model.closed_time = close
+
+    summary_seconds = (str2datetime(close) - str2datetime(start)).total_seconds()
+
+    same_day = is_same_day(start, close)
+    if same_day:
+        date_list = [[str2datetime(start), str2datetime(close)]]
+    else:
+        date_list = get_date_list_from_more_than_one_day(start, close)
+
+    delta_list = []
+    for start_close in date_list:
+        start, close = start_close
+        delta_time = calc(start, close)
+        delta_list.append(delta_time)
+    delta_summary_seconds = sum(delta_list)
+    actual_seconds = summary_seconds - delta_summary_seconds
+
+    sla_model.total_time_delta_min = summary_seconds / 60
+    sla_model.total_time_delta_hour = sla_model.total_time_delta_min / 60
+    sla_model.actual_time_delta_hour = actual_seconds / 3600
+    return sla_model
+
+
+def calc_multiple_events(date_list):
+    # date_list = [
+    #     ['2022/5/16 08:28:05', '2022/5/16 17:06:22'],
+    #     ['2022/5/16 16:55:17', '2022/5/17 10:46:11'],
+    #     ['2022/5/20 16:54:42', '2022/5/23 17:15:02'],
+    #     ['2022/5/3 09:30:32', '2022/5/5 14:05:41'],
+    # ]
+
+    r = []
+    for start_close in date_list:
+        start, close = start_close
+        sla_model: SlaModel = calc_single_event(start, close)
+        r.append([
+            sla_model.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+            sla_model.closed_time.strftime("%Y-%m-%d %H:%M:%S"),
+            f'{sla_model.total_time_delta_min:.0f}',
+            f'{sla_model.total_time_delta_hour:.1f}',
+            f'{sla_model.actual_time_delta_hour:.1f}',
+        ])
+    return r
+
+
+def read_excel(file_excel, sheet_name):
+    wb = openpyxl.load_workbook(file_excel)
+    sheet_names = wb.sheetnames
+    if sheet_name not in sheet_names:
+        ws = wb.active
+    else:
+        ws = wb[sheet_name]
+
+    max_row = ws.max_row
+    max_col = ws.max_column
+
+    result = []
+    for row_index in range(max_row):
+        row_index = row_index + 1
+
+        row_list = []
+        for col_index in range(max_col):
+            col_index = col_index + 1
+
+            cell_value = ws.cell(row=row_index, column=col_index).value
+            row_list.append(cell_value)
+        result.append(row_list)
+    wb.close()
+    return result
+
+
+def write_to_excel(file_excel, data_list):
+    data_list = list(data_list)
+
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    for row_index, row_list in enumerate(data_list):
+        row_index = row_index + 1
+
+        for col_index, cell_value in enumerate(row_list):
+            col_index = col_index + 1
+
+            ws.cell(row=row_index, column=col_index, value=cell_value)
+
+    wb.save(file_excel)
 
 
 def main():
-    calc('', '')
+    excel_list = read_excel(BREAK_DICT.input_excel_file_name, BREAK_DICT.input_excel_sheet_name)
+    date_list = []
+    for row in excel_list[1:]:
+        start, close = row[0], row[1]
+        if start and close:
+            date_list.append([start, close])
+    data_list = [excel_list[0]]
+    result_list = calc_multiple_events(date_list)
+    data_list.extend(result_list)
+    write_to_excel(BREAK_DICT.output_excel_file_name, data_list)
 
 
 if __name__ == '__main__':
